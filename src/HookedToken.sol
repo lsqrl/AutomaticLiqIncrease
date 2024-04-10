ad1190cff46bfb257c675b412711317d18bb01fa
lsqrl <lsqrl91@gmail.com>
2024-04-10 23:42:19 +0200

    erc20 install

--- src/HookedToken.sol
+++ src/HookedToken.sol
@@ -1,8 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-//import erc20
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
 
 contract HookedToken {
     
