6e5fc3af917bda1951fcb3eeacaff8f7f850c5e5
lsqrl <lsqrl91@gmail.com>
2024-04-12 17:28:11 +0200

    first sketch

--- src/HookedToken.sol
+++ src/HookedToken.sol
@@ -2,14 +2,82 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
-import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+import './interfaces/external/INonfungiblePositionManager.sol';
 
-contract HookedToken {
+contract HookedToken is ERC20 {
     
+    INonfungiblePositionManager public immutable nonfungiblePositionManager;
+
+    address public uniswapPool;
+    uint256 public tokenId;
+
+    constructor(address _nfpm) ERC20("HookedToken", "HT") {
+        nonfungiblePositionManager = INonfungiblePositionManager(_nfpm);
+    }
+
+    function _mintPosition() internal {
+        // mint a position on nonfungiblePositionManager
+        INonfungiblePositionManager.MintParams memory mintParams = INonfungiblePositionManager.MintParams({
+            token0: address(0),
+            token1: address(0),
+            fee: 3000,
+            tickLower: -887220,
+            tickUpper: 887200,
+            amount0Desired: 0,
+            amount1Desired: 0,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+        nonfungiblePositionManager.mint(mintParams);
+    }
+
+    function _increaseLiquidty(uint256 amount0, uint256 amount1) internal {
+        INonfungiblePositionManager.IncreaseLiquidityParams memory increaseLiquidityParams = INonfungiblePositionManager.IncreaseLiquidityParams({
+            tokenId: tokenId,
+            amount0Desired: amount0,
+            amount1Desired: amount1,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: block.timestamp
+        });
+        nonfungiblePositionManager.increaseLiquidity(increaseLiquidityParams);
+    }
+
+    function _decreaseLiquidity(uint256 liquidity) internal {
+        INonfungiblePositionManager.DecreaseLiquidityParams memory decreaseLiquidityParams = INonfungiblePositionManager.DecreaseLiquidityParams({
+            tokenId: tokenId,
+            liquidity: 0,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: block.timestamp
+        });
+        nonfungiblePositionManager.decreaseLiquidity(decreaseLiquidityParams);
+    }
+
+    function _burnPosition(uint256 tokenId) internal {
+        nonfungiblePositionManager.burn(tokenId);
+    }
+
+    function _update(address from, address to, uint256 value) internal override {
+        // recall that the order is Uniswap -> User and afterwards User -> Uniswap
+        // and Uniswap has a callback check for the tokens it receives
+        // moreover, the transfer is to the recipient, but from the router
+        // thus, we only insert the hook on token purchases, not sales
+
+        // we first make the transfer normally
+        super._update(from, to, value);
+
+        if(from == uniswapPool) {
+            // the user is buying tokens from Uniswap: 5% is used to increase liquidity
+            // notice that this has a negative impact on the spot price
+            // but a positive impact on the floor price
+            uint256 tax = value / 20;
+            super._update(to, address(this), tax);
+            _increaseLiquidty(tax, tax);
+        } 
+
+    }
 }
--- test/HookedToken.t.sol
+++ test/HookedToken.t.sol
@@ -7,7 +7,17 @@ import {HookedToken} from "../src/HookedToken.sol";
 contract HookedTokenTest is Test {
     HookedToken public hookedTOken;
 
+    // arbitrum address 0xC36442b4a4522E871399CD717aBDD847Ab11FE88
+    // arbitrum sepolia address 0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65
+
+    address internal constant nonfungiblePositionManager = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88; 
+
+    uint256 blockNumber = 199708393;
+    string internal constant rpcUrl = "ARBITRUM_RPC_URL";
+
     function setUp() public {
-        hookedTOken = new HookedToken();
+        uint256 forkId = vm.createFork(vm.envString(rpcUrl), blockNumber);
+        vm.selectFork(forkId);
+        hookedTOken = new HookedToken(nonfungiblePositionManager); 
     }
 }
